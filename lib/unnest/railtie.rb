9ba9d7fcb2a1310a972e52a2f82c7f705ffd8066
Peter Woo <petersnowdonwoo@gmail.com>
2014-10-03 17:07:26 -0700

    Trick is only valid for integer id columns

--- lib/unnest/railtie.rb
+++ lib/unnest/railtie.rb
@@ -8,28 +8,29 @@ module Unnest
         if opts.is_a?(Hash)
           opts = opts.clone
           opts.keys.each do |col|
-            if opts[col].is_a?(Array) && opts[col].size > 1
+            if "#_{col}"[-3..-1] == 'id' && opts[col].is_a?(Array) && opts[col].size > 1
               unnests[col] = opts.delete(col)
             end
           end
         elsif opts.is_a?(Arel::Nodes::In) && opts.right.is_a?(Array) && opts.right.size > 1 &&
-              opts.left.is_a?(Arel::Attributes::Attribute)
+            opts.left.is_a?(Arel::Attributes::Attribute)
           unnests[opts.left.name] = opts.right
+          opts = nil
         end
 
         relation = clone
-        relation.where_values += build_where(opts, rest)
+        relation.where_values += build_where(opts, rest) if opts
 
-	i = 0
-	unless unnests.empty?
+        i = 0
+        unless unnests.empty?
           unnests.each do |col, values|
             unnest = "unnest(array[#{values.map(&:to_i).join(',')}])"
             tmp = "_unnest_vals#{i}"
             relation = relation.joins("INNER JOIN #{unnest} #{tmp} ON #{table_name}.#{col} = #{tmp}")
             i = i + 1
           end
-	end
-	relation
+        end
+        relation
       end
     end
   end
