7e25c170abaa6c80f49cd48859d344f3a08464b7
Peter Woo <petersnowdonwoo@gmail.com>
2014-10-03 17:42:05 -0700

    Reorganize

--- lib/unnest/railtie.rb
+++ lib/unnest/railtie.rb
@@ -1,9 +1,22 @@
 module Unnest
   class Railtie < Rails::Railtie
     initializer('unnest.patches') do
+      ActiveRecord::Relation.send(:alias_method, :original_where, :where)
+
       ActiveRecord::Relation.send(:define_method, :where) do |opts, *rest|
         return self if opts.blank?
 
+        opts, unnests = separate_unnest_options(opts)
+        unnests = construct_unnest_join_clauses(unnests)
+
+        if unnests.blank?
+          original_where(opts, rest)
+        else
+          original_where(opts, rest).joins(unnests)
+        end
+      end
+
+      ActiveRecord::Relation.send(:define_method, :separate_unnest_options) do |opts|
         unnests = {}
         if opts.is_a?(Hash)
           opts = opts.clone
@@ -17,20 +30,19 @@ module Unnest
           unnests[opts.left.name] = opts.right
           opts = nil
         end
+        [opts, unnests]
+      end
 
-        relation = clone
-        relation.where_values += build_where(opts, rest) if opts
-
-        i = 0
+      ActiveRecord::Relation.send(:define_method, :construct_unnest_join_clauses) do |unnests|
+        joins = []
         unless unnests.empty?
           unnests.each do |col, values|
             unnest = "unnest(array[#{values.map(&:to_i).join(',')}])"
-            tmp = "_unnest_vals#{i}"
-            relation = relation.joins("INNER JOIN #{unnest} #{tmp} ON #{table_name}.#{col} = #{tmp}")
-            i = i + 1
+            tmp = "_unnest_vals#{joins.size}"
+            joins << "INNER JOIN #{unnest} #{tmp} ON #{table_name}.#{col} = #{tmp}"
           end
         end
-        relation
+        joins.join(' ')
       end
     end
   end
